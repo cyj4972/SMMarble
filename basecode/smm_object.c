d1d037f8bfff9323d713d13df9f7ede1286d4d05
cyj4972 <144991384+cyj4972@users.noreply.github.com>
2023-12-15 10:56:25 +0900

    SMMarble_1

---
+++ basecode/smm_object.c
@@ -0,0 +1,103 @@
+//
+//  smm_node.c
+//  SMMarble
+//
+//  Created by Juyeop Kim on 2023/11/05.
+//
+
+#include "smm_common.h"
+#include "smm_object.h"
+#include <string.h>
+
+#define MAX_NODETYPE    7
+#define MAX_GRADE       9
+#define MAX_NODE        100
+
+static char smmNodeName[SMMNODE_TYPE_MAX][MAX_CHARNAME] = {
+	"강의",
+	"식당",
+	"실험실",
+	"집",
+	"실험실로 이동",
+	"음식찬스",
+	"축제"
+};
+
+char *smmObj_getTypeName(int type)
+{
+	return (char*)smmNodeName[type];
+} 
+
+
+//1. 구조체 형식 정의
+typedef struct smmObject{
+	char name[MAX_CHARNAME];
+	smmObjType_e objType;
+	int type;
+	int credit;
+	int energy;
+	smmObjGrade_e grade;
+}smmObject_t;
+
+//2. 구조체 배열 변수 선언 
+// static smmObject_t smm_node[MAX_NODE];
+//static int smmObj_noNode=0;
+
+
+//3. 기존 함수 수정 및 새로 작성 
+//object generation
+void smmObj_genObject(char* name, smmObjType_e objType, int type, int credit, int energy, smmObjGrade_e grade)
+{ 
+	smmObject_t* ptr;
+	
+	ptr = (smmObject_t*)malloc(sizeof(smmObject_t));
+	
+	strcpy(ptr->name, name);
+	ptr->objType = objType;
+	ptr->type = type;
+	ptr->credit = credit;
+	ptr->energy = energy;
+	ptr->grade = grade;
+	
+	return ptr;
+}
+
+char* smmObj_getNodeName(void* obj)
+{	
+	smmObject_t* ptr = (smmObject_t*)obj;
+	 
+    return ptr->name;
+}
+
+int smmObj_getNodeType(int node_nr)
+{
+    return smm_node[node_nr].type;
+}
+
+int smmObj_getNodeCredit(int node_nr)
+{
+	return smm_node[node_nr].credit;
+}
+
+int smmObj_getNodeEnergy(int node_nr)
+{
+	return smm_node[node_nr].energy;
+}
+
+#if 0
+//member retrieving
+
+
+
+//element to string
+char* smmObj_getNodeName(smmNode_e type)
+{
+    return smmNodeName[type];
+}
+
+char* smmObj_getGradeName(smmGrade_e grade)
+{
+    return smmGradeName[grade];
+}
+
+#end if
---
+++ basecode/smm_object.h
@@ -0,0 +1,66 @@
+//
+//  smm_object.h
+//  SMMarble object
+//
+//  Created by Juyeop Kim on 2023/11/05.
+//
+
+#ifndef smm_object_h
+#define smm_object_h
+
+//유형별 번호 
+#define SMMNODE_TYPE_LECTURE		0		//강의 
+#define SMMNODE_TYPE_RESTAURANT		1		//식당
+#define SMMNODE_TYPE_LABORATORY		2		//실험실 
+#define SMMNODE_TYPE_HOME			3		//집 
+#define SMMNODE_TYPE_GOTOLAB		4		//실험실로 이동 
+#define SMMNODE_TYPE_FOODCHANCE		5		//식사 
+#define SMMNODE_TYPE_FESTIVAL		6		//축제 
+
+#define SMMNODE_TYPE_MAX			7
+
+typedef enum smmObjType{
+	smmObjType_board = 0,
+	smmObjType_card,
+	smmObjType_grade
+}smmObjType_e;
+
+/* node type :
+    lecture,
+    restaurant,
+    laboratory,
+    home,
+    experiment,
+    foodChance,
+    festival
+*/
+
+
+typedef enum smmObjGrade{
+	smmObjType_AP = 0,
+	smmObjType_A0,
+	smmObjType_AM,
+	smmObjType_BP,
+	smmObjType_B0,
+	smmObjType_BM,
+	smmObjType_CP,
+	smmObjType_C0,
+	smmObjType_CM
+}smmObjGrade_e;
+
+
+
+//object generation
+void smmObj_genObject(char* name, smmObjType_e objType, int credit, int energy, smmObjGrade_e grade);
+
+//member retrieving
+char* smmObj_getNodeName(int node_nr);
+int smmObj_getNodeType(int node_nr);
+int smmObj_getNodeCredit(int node_nr);
+int smmObj_getNodeEnergy(int node_nr);
+
+//element to string
+char* smmObj_getTypeName(int type);
+
+
+#endif /* smm_object_h */
